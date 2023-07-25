fab767629ae994cdc2fd1c741b6e316fb07aee93
Anna Kapitanova <totoshechka19@gmail.com>
2023-07-25 15:19:17 +0300

    Сделала расчет общей стоимости в модальном окне при собирании готового напитка

--- js/script.js
+++ js/script.js
@@ -69,7 +69,7 @@ const scrollService = {
 };
 
 // Скрипт для открытия/закрытия модальных окон
-const modalController = ({ modal, btnOpen, time = 300 }) => {
+const modalController = ({ modal, btnOpen, time = 300, open, close }) => {
     const buttonElems = document.querySelectorAll(btnOpen);
     const modalElem = document.querySelector(modal);
 
@@ -90,13 +90,19 @@ const modalController = ({ modal, btnOpen, time = 300 }) => {
             setTimeout(() => {
                 modalElem.style.visibility = "hidden";
                 scrollService.enabledScroll();
+                if (close) {
+                    close();
+                }
             }, time);
 
             window.removeEventListener("keydown", closeModal);
         }
     };
 
-    const openModal = () => {
+    const openModal = (e) => {
+        if (open) {
+            open({ btn: e.target} );
+        }
         modalElem.style.visibility = "visible";
         modalElem.style.opacity = "1";
         window.addEventListener("keydown", closeModal);
@@ -172,6 +178,47 @@ const calculateMakeYourOwn = () => {
     handlerChange();
 }
 
+const calculateAdd = () => {
+    const modalAdd = document.querySelector(".modal__add");
+    const formAdd = document.querySelector(".make__form-add");
+    const makeTitle = modalAdd.querySelector(".make__title");
+    const makeInputTitle = modalAdd.querySelector(".make__input-title");
+    const makeTotalPrice = modalAdd.querySelector(".make__total-price");
+    const makeInputStartPrice = modalAdd.querySelector(".make__input-start-price");
+    const makeInputPrice = modalAdd.querySelector(".make__input-price");
+    const makeTotalSize = modalAdd.querySelector(".make__total-size");
+    const makeInputSize = modalAdd.querySelector(".make__input-size");
+
+    const handlerChange = () => {
+        const totalPrice = calculateTotalPrice(formAdd, +makeInputStartPrice.value);
+        makeInputPrice.value = totalPrice;
+        makeTotalPrice.textContent = `${totalPrice} ₽`;
+
+    }
+
+    formAdd.addEventListener("change", handlerChange);
+
+    const fillInForm = (data) => {
+        makeTitle.textContent = data.title;
+        makeInputTitle.value = data.title;
+        makeTotalPrice.textContent = `${data.price} ₽`;
+        makeInputStartPrice.value = data.price;
+        makeInputPrice.value = data.price;
+        makeTotalSize.textContent = data.size;
+        makeInputSize.value = data.size;
+        handlerChange();
+    }
+
+    const resetForm = () => {
+        makeTitle.textContent = "";
+        makeTotalPrice.innerHTML = "";
+        makeTotalSize.textContent = "";
+        formAdd.reset();
+    }
+
+    return {fillInForm, resetForm};
+}
+
 // Получение html элемента goods__list
 const init = async () => {
     modalController({
@@ -199,9 +246,17 @@ const init = async () => {
 
     goodsListElem.append(...cardsCocktail);
 
+    const {fillInForm, resetForm} = calculateAdd();
+
     modalController( {
         modal: ".modal__add",
-        btnOpen: ".cocktail__btn_add"
+        btnOpen: ".cocktail__btn_add",
+        open({btn}) {
+            const id = btn.dataset.id;
+            const item = data.find( item => item.id.toString() === id );
+            fillInForm(item);
+        },
+        close: resetForm,
     });
 };
 
